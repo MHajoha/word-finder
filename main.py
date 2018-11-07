40391ef1125be4eab98be61695b285a1c8ca7d16
MHajoha <mhajoha@gmail.com>
2018-11-07 19:22:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import argparse
+import re
+from enum import Enum, auto
+
+
+class SourceFormat(Enum):
+    LINES = auto()
+    SSV = auto()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--word-source", "-s", type=argparse.FileType("r"),
+                    default="./openthesaurus.txt")
+parser.add_argument("--source-format", "-f", choices=dir(SourceFormat))
+parser.add_argument("string")
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    pattern = re.compile(r"^(\(.*\))?\s*(?P<word>" +
+                         r"\w*" + r"\w*".join(args.string) + r"\w*" +
+                         r")\s*(\(.*\))?$", re.IGNORECASE)
+
+    words = []
+    for line in args.word_source:
+        if line.startswith("#"):
+            continue
+
+        for word in line.split(";"):
+            match = pattern.match(word)
+            if match:
+                words.append(match.group("word"))
+            else:
+                continue
+
+    for word in words:
+        print(word)
